c366851eba8f24bde0461ef9f5b4ce82e54877e2
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2022-07-17 21:30:02 +0530

    initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav class="navbar">
+      <img src="/src/images/airbnb.png" alt="" />
+    </nav>
+  );
+}
+
+export default Navbar;
