33edd5145f54d7d4fac8cf1e8f0ea868ddcdc61f
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2022-07-17 22:43:30 +0530

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero--image">
+        <img src="/src/images/airbnb-exp.png" />
+      </div>
+      <div className="hero--title">
+        <h1>Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
