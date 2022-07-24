f5ca34fdde90d4f55d7d6727259a7116fc3b2902
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2022-07-24 14:15:52 +0530

    data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map((card) => (
+    <Card
+      key={card.id}
+      img={card.coverImg}
+      title={card.title}
+      rating={card.stats.rating}
+      reviewCount={card.stats.reviewCount}
+      price={card.price}
+      location={card.location}
+    />
+  ));
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <section className="cards">{cards}</section>
     </div>
   );
 }
